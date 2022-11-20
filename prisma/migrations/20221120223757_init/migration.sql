ede3c0ee7e7cb5c246c3626c8bf554e3f8647c18
Bogdan B Peterson <bogdanbpeterson@gmail.com>
2022-11-21 01:55:24 +0200

    feat: switched to postgresql

--- prisma/migrations/20221120223757_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Chat" (
-    "chatId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "encounters" INTEGER NOT NULL DEFAULT 0
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Chat_chatId_key" ON "Chat"("chatId");
