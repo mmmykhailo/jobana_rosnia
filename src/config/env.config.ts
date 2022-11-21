e2a097addd51753c7487d38b7d57ebce02403c19
Bogdan B Peterson <bogdanbpeterson@gmail.com>
2022-11-21 12:08:47 +0200

    fix: no env file in prod

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 interface ENV {
   BOT_TOKEN: string | undefined;
