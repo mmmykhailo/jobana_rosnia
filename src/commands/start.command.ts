f8c74efb975a3a46bdbe4169d2c18bbc1bb231f5
Bogdan B Peterson <118484531+bogdanbpeterson@users.noreply.github.com>
2022-12-05 15:05:39 +0000

    Change start behaviour for groups (#11)
    * chore: updated start handler for groups
    
    * chore: updated version; pinned node version

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -1,8 +1,9 @@
+import { chatExists } from '../models/chat.model';
 import type { TelegrafStartCommandHandler } from '../types';
 import { isChatGroup } from '../utils/isChatGroup';
 import { isChatPrivate } from '../utils/isChatPrivate';
 
-export const startCommand: TelegrafStartCommandHandler = (ctx) => {
+export const startCommand: TelegrafStartCommandHandler = async (ctx) => {
   if (isChatPrivate(ctx.message.chat)) {
     ctx.reply(
       'Розпочни подорож русофобією!\nДодай мене в групу та починай їбати русню'
@@ -10,9 +11,16 @@ export const startCommand: TelegrafStartCommandHandler = (ctx) => {
   }
 
   if (isChatGroup(ctx.message.chat)) {
-    ctx.reply(
-      'Розпочніть подорож русофобією!\nВідправте повідомлення з йобаною руснею',
-      { reply_to_message_id: ctx.message.message_id }
-    );
+    if (await chatExists(ctx.message.chat.id)) {
+      ctx.reply(
+        'Відправляйте повідомлення з йобаною руснею та змагайтеся з іншими чатами у вашій русофобії!',
+        { reply_to_message_id: ctx.message.message_id }
+      );
+    } else {
+      ctx.reply(
+        'Розпочніть подорож русофобією!\nВідправте повідомлення з йобаною руснею та почніть змагатися з іншими чатами у русофобії',
+        { reply_to_message_id: ctx.message.message_id }
+      );
+    }
   }
 };
