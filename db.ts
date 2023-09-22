f0081697280513cdd3e566008589cde8ec3949eb
Bogdan B Peterson <github@bogdanbpeterson.com>
2023-09-22 23:00:25 +0300

    fix: ssl mode, types, and logging

--- db.ts
+++ db.ts
@@ -1,4 +1,12 @@
 import postgres from "postgresjs/mod.js";
 import { DATABASE_URL } from "./env.ts";
 
-export const db = postgres(DATABASE_URL);
+export const db = postgres(DATABASE_URL, {
+  ssl: true,
+  types: {
+    bigint: postgres.BigInt,
+  },
+  debug(connection, query, parameters, paramTypes) {
+    console.log(connection, query, parameters, paramTypes);
+  },
+});
